756a0b72168509406e2221398ac3a4df4c440e7f
NOrangeeroli <1842850321@qq.com>
2022-07-27 09:44:03 -0400

    modified:   create_dataset.py

--- create_dataset.py
+++ create_dataset.py
@@ -15,6 +15,7 @@ limit = {'train': max(total_lim*0.7,1), \
         'overfit': 2}
 
 split_list = ['train','test','val','overfit']
+split_list = ['overfit']
 for split in split_list:
     if not os.path.exists("render/render_"+set_name+'/'+split):
         os.makedirs("render/render_"+set_name+'/'+split)
@@ -29,12 +30,13 @@ index_train = np.random.choice(np.arange(n_obj), int(0.7 * n_obj), replace=False
 index_test_and_val = np.setdiff1d(np.arange(n_obj), index_train, True)
 index_val = index_test_and_val[np.random.choice(np.arange(index_test_and_val.size), int(0.5*index_test_and_val.size), replace=False)]
 index_test = np.setdiff1d(index_test_and_val, index_val)
-index_overfit = np.random.choice(np.arange(n_obj), n_obj, replace=False)
+index_overfit = np.array([0,1])
 rot_z=[-110, -90, -60, -30, -10]
 rot_x=[30, 45, 60, 75, 90]
 
 
 index={'train': index_train, 'test': index_test, 'val': index_val, 'overfit': index_overfit}
+index={'overfit': index_overfit}
 for split in split_list:
     count = 0 
     with open('split_txt/split_txt_'+set_name+ '/' + split+'_split.txt','w') as f:
